22cdb744f696423b6c2729f47f931b425f6d7041
Yi Cheng Lee <leeyicheng10@gmail.com>
2021-08-15 09:26:00 +0800

    commit2

---
+++ passengerCounter/index.js
@@ -0,0 +1,11 @@
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints += 50;
+console.log(bonusPoints);
+
+bonusPoints -= 75;
+console.log(bonusPoints);
+
+bonusPoints +=45;
+console.log(bonusPoints);
